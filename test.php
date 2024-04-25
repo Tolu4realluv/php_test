12024ec4f116a19995a2c110ae37cf01c9cd3a67
Tolu4realluv <toluwase2@gmail.com>
2024-04-25 13:57:47 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "Lucid-Show";
+$username = "user";
+$password = "Mohabbatein1.";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+?>
